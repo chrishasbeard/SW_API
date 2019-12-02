7bddda1333f0b9a54bc4d325ee2bc5c89fa1fe8c
chrishasbeard <chrisholmes16@gmail.com>
2019-12-02 08:57:36 -0500

    json parse error, needs correction

---
+++ StarWars API/Controllers/StarWarsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using StarWars_API.Models;
+
+namespace StarWars_API.Controllers
+{
+    public class StarWarsController : Controller
+    {
+        public readonly HttpClient _client;
+
+        public StarWarsController(IHttpClientFactory client)
+        {
+            _client = client.CreateClient();
+            _client.BaseAddress = new Uri("https://swapi.co/api");
+            
+        }
+
+        public async Task<IActionResult> DisplayThing(string tag)
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> DisplayPlanet()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> DisplayPlanet(string tag)
+        {
+            var response = await _client.GetAsync($"planet/:{tag}");
+            var results = await response.Content.ReadAsStringAsync();
+
+            HttpContext.Session.SetString("Planet", results);
+
+            var planets = JsonConvert.DeserializeObject<PlanetRoot>(results);
+
+            return View(planets);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DisplayPeople()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> DisplayPeople(int tag)
+        {
+            var response = await _client.GetAsync($"/people/:{tag}/");
+            var results = await response.Content.ReadAsStringAsync();
+
+            HttpContext.Session.SetString("Persons", results);
+
+            var people = JsonConvert.DeserializeObject<PeopleRoot>(results);
+
+            return View(results); 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ StarWars API/Models/JediText.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWars_API.Models
+{
+
+    public class Rootobject
+    {
+        public string people { get; set; }
+        public string planets { get; set; }
+        public string films { get; set; }
+        public string species { get; set; }
+        public string vehicles { get; set; }
+        public string starships { get; set; }
+    }
+
+}
